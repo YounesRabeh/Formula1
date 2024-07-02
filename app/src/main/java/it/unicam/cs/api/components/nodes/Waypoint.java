8b06fdc2ea01d658dd4fcc3b20063dbbff79d0a5
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-07-02 11:36:44 +0200

    reformat

---
+++ app/src/main/java/it/unicam/cs/api/components/nodes/Waypoint.java
@@ -0,0 +1,19 @@
+package it.unicam.cs.api.components.nodes;
+
+import javafx.geometry.Point2D;
+
+public class Waypoint extends Point2D {
+    /**
+     * Creates a new Waypoint with the specified coordinates.
+     *
+     * @param x the x coordinate of the point
+     * @param y the y coordinate of the point
+     */
+    public Waypoint(double x, double y) {
+        super(x, y);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/it/unicam/cs/gui/map/TrackCanvas.java
@@ -0,0 +1,10 @@
+package it.unicam.cs.gui.map;
+
+import javafx.scene.canvas.Canvas;
+
+public class TrackCanvas extends Canvas {
+
+
+
+
+}
