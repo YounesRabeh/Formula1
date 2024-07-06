00fdfbdf15ce00bbe1f91e0cb4ad40c4b697ee67
Younes Rabeh <93033194+YounesRabeh@users.noreply.github.com>
2024-07-06 15:32:43 +0200

    debug

--- app/src/main/java/it/unicam/cs/api/components/container/DebugData.java
+++ app/src/main/java/it/unicam/cs/api/components/container/DebugData.java
@@ -2,7 +2,7 @@ package it.unicam.cs.api.components.container;
 
 
 public interface DebugData {
-    String PATH = "/it/unicam/cs/test.txt";
+    String PATH = "/it/unicam/cs/test.f1m";
     int WIDTH = 1000;
     int HEIGHT = 1000;
     int WAYPOINT_LVL = 2;
